8efc042067336ab40b625294a910d80cade05e4c
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-10-28 22:01:55 +0300

    Add form in login page

--- react-admin/src/components/login/Login.jsx
+++ react-admin/src/components/login/Login.jsx
@@ -1,5 +1,29 @@
 import "./style.css";
 
 export default function Login() {
-  return <>Login</>;
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <label className="login-label">Login:</label>
+        <input
+          type="text"
+          className="login-input"
+          value={undefined}
+          onChange={() => {}}
+        />
+
+        <label className="login-label">Password:</label>
+        <input
+          type="password"
+          className="login-input"
+          value={undefined}
+          onChange={() => {}}
+        />
+
+        <button className="login-button" type="button" onClick={() => {}}>
+          Enter
+        </button>
+      </div>
+    </div>
+  );
 }
